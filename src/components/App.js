7fe1546b64cf4985acb99b3a19e42c19dc979e87
kgrif113 <91581102+kgrif113@users.noreply.github.com>
2023-02-01 14:44:09 -0500

    styling

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,18 @@ import { Link, Outlet } from "react-router-dom";
 const App = () => {
   return (
     <>
-    <div className="logobox">
+      <div className="app">
+      <div className="logobox">
         <div className="logo">
-            <img src="Section 1.png" />
+            <img className="logoimg" src="Section 1.png" />
         </div>
       </div>
-      <div className="app">
         <nav className="navigator">
           <Link to="/" className="home-nav">
-            <img src="home.svg"/>
+            <img src="icons8-home-69.png"/>
           </Link>
           <Link to="Login" className="login-nav">
-          <img src="log-in.svg"/>
+          <img src="icons8-checkmark-69.png"/>
           </Link>
           <Link to="Register" className="register-nav">
           <img src="register.svg"/>
